9d7c5114742c66fe67f3dc2911466652ccb217d7
hashimtu <hashimtu@gmail.com>
2025-06-19 23:07:29 +0000

    final replit to github

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 Jobs= [
@@ -10,7 +10,8 @@ Jobs= [
 @app.route('/')
 def hello_world():
   return render_template('home.html', jobs=Jobs)
-
-
+@app.route('/jobs')
+def list_jobs():
+  return jsonify(Jobs)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
