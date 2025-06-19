e53b1342cb7fa4574c52b5f4f50fe1ae28453aee
hashimtu <hashimtu@gmail.com>
2025-06-19 19:48:46 +0000

    website job rendered to html

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+Jobs= [
+  {'id':1, 'title':'Data Analyst', 'location':'Bengaluru', 'salary':'Rs. 10,00,000'},
+  {'id':2, 'title':'Data Scientist', 'location':'Delhi', 'salary':'Rs. 15,00,000'},
 
+]
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=Jobs)
 
 
 if __name__ == '__main__':
