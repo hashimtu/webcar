46b1ccb7e0787bc43f7b8308009b1378af4613dc
hashimtu <hashimtu@gmail.com>
2025-06-22 09:40:16 +0000

    ok

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
 
-engine = create_engine(
-    'mysql+pymysql://sql12786184:Um2DwxGJLs@sql12.freesqldatabase.com:3306/sql12786184'
+engine = create_engine('mysql+pymysql://sql12786184:Um2DwxGJLs@sql12.freesqldatabase.com:3306/sql12786184'
 )
