bbd28a43bb14a02b0919d282eec26088c82d7f00
hashimtu <hashimtu@gmail.com>
2025-06-22 11:00:52 +0000

    back

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, jsonify
+from sqlalchemy import text
+from database import engine
 
 app = Flask(__name__)
-Jobs= [
-  {'id':1, 'title':'Data Analyst', 'location':'Bengaluru', 'salary':'Rs. 10,00,000'},
-  {'id':2, 'title':'Data Scientist', 'location':'Delhi', 'salary':'Rs. 15,00,000'},
 
-]
+
+def loadjobsfromdb():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+    return jobs
+
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html', jobs=Jobs)
+  return render_template('home.html', jobs=jobs)
+
+
 @app.route('/jobs')
 def list_jobs():
   return jsonify(Jobs)
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
 engine = create_engine(
     'mysql+pymysql://sql12786184:Um2DwxGJLs@sql12.freesqldatabase.com:3306/sql12786184'
