32c578bed1f4383802fd9d56503648117ab5c5fe
hashimtu <hashimtu@gmail.com>
2025-06-22 09:45:18 +0000

    connection to database

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 engine = create_engine('mysql+pymysql://sql12786184:Um2DwxGJLs@sql12.freesqldatabase.com:3306/sql12786184'
 )
+result = connection.execute(text("SELECT * FROM your_table_name"))
+for row in result:
+    print(row)
